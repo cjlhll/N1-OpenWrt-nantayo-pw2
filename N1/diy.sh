a7c9528e47bc782002c522a92836dfa99c189896
AJ <983097239@qq.com>
2024-12-19 13:22:15 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -7,14 +7,14 @@ sed -i 's/192.168.1.1/192.168.5.2/g' package/base-files/files/bin/config_generat
 sed -i 's/^root:.*:/root:$1$KVHNuqbv$4X2BPbtsXn2AApknHIn38.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # => alist
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
-git clone https://github.com/sbwml/luci-app-alist package/alist
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+#git clone https://github.com/sbwml/luci-app-alist package/alist
 
 # => 阿里ddns
-git clone https://github.com/chenhw2/luci-app-aliddns.git package/luci-app-aliddns
+#git clone https://github.com/chenhw2/luci-app-aliddns.git package/luci-app-aliddns
 
 # => passwall2
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git -b main package/passwall_package
-git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+#git clone https://github.com/xiaorouji/openwrt-passwall-packages.git -b main package/passwall_package
+#git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
 
